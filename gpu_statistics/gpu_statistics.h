e5edc3ef7a83cf1b470b5cdb1092275016c46275
Burakherdogan <erdoganburakhasan@gmail.com>
2024-03-30 10:53:07 +0300

    updated gpu header file

---
+++ gpu_statistics/gpu_statistics.h
@@ -0,0 +1,14 @@
+#ifndef __GPU_STATISTICS__
+#define __GPU_STATISTICS__
+
+// Function to retrieve the current clock speed of the GPU.
+int show_gpu_clock_speed();
+// Function to retrieve the current temperature of the GPU.
+int show_gpu_temp();
+// Function to retrieve the current voltage of the GPU.
+int show_gpu_voltage();
+// Function to retrieve the current GPU usage rate.
+int show_gpu_usage_rate();
+
+
+#endif // !__GPU_STATISTICS__
