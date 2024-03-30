365c5987cca7d24dfe7f42a31a9c1521895a63ea
Burakherdogan <erdoganburakhasan@gmail.com>
2024-03-30 13:16:48 +0300

    updated disks header file

--- disks_statistics/disks_statistics.h
+++ disks_statistics/disks_statistics.h
@@ -1,1 +1,11 @@
+#ifndef __DISKS_STATISTICS__
+#define __DISKS_STATISTICS__
 
+// This function retrieves the temperature of the disk drive.
+int show_disk_temp();
+// This function retrieves the count of power cycles that the disk drive has experienced.
+int show_power_cycle_count();
+// This function calculates the disk usage percentage of the file system.
+double show_disk_usage();
+
+#endif // !__DISKS_STATISTICS__
