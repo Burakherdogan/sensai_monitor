fc35e5ee5f64627a861d12f64400bec92cffe538
Burakherdogan <erdoganburakhasan@gmail.com>
2024-03-30 10:39:38 +0300

    updated fan header file

---
+++ fan_statistics/fan_statistics.h
@@ -0,0 +1,9 @@
+#ifndef __FAN_STATISTICS__
+#define __FAN_STATISTICS__
+
+// Retrieves the current speed of the fan.
+int show_fan_speed();
+// Retrieves the name of the fan.
+char* show_fan_name();
+
+#endif // !__FAN_STATISTICS_
