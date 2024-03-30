be7bee4675325a3c29644ea6f68bbf2f23c3d501
Burakherdogan <erdoganburakhasan@gmail.com>
2024-03-30 10:58:19 +0300

    updated memory header file

---
+++ memory_statistics/memory_statistics.h
@@ -0,0 +1,14 @@
+#ifndef __MEMORY_STATISTICS__
+#define __MEMORY_STATISTICS__
+
+// Function to retrieve the amount of used memory in the system.
+int show_used_memory();
+// Function to retrieve the amount of free memory in the system.
+int show_free_memory();
+// Function to retrieve the amount of cache memory in the system.
+int show_cache_memory();
+// Function to retrieve the amount of swap space used in the system.
+int show_swap_();
+
+
+#endif // !__MEMORY_STATISTICS__
